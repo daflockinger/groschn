2a79bc9c6fd9a832674eb9773a3e0af36f5337fe
daflockinger <florian.mitterbauer@gmx.net>
2018-11-16 12:02:48 +0100

    removed not needed methods from some swagger-generated DTO's.

--- blockchain/src/main/java/com/flockinger/groschn/blockchain/api/dto/Error.java
+++ blockchain/src/main/java/com/flockinger/groschn/blockchain/api/dto/Error.java
@@ -24,11 +24,6 @@ public class Error   {
   @JsonProperty("message")
   private String message = null;
 
-  public Error code(Integer code) {
-    this.code = code;
-    return this;
-  }
-
   /**
    * Get code
    * @return code
@@ -44,26 +39,11 @@ public class Error   {
     this.code = code;
   }
 
-  public Error fields(Map<String, String> fields) {
-    this.fields = fields;
-    return this;
-  }
-
-  public Error putFieldsItem(String key, String fieldsItem) {
-    if (this.fields == null) {
-      this.fields = new HashMap<String, String>();
-    }
-    this.fields.put(key, fieldsItem);
-    return this;
-  }
-
   /**
    * Invalid fields.
    * @return fields
   **/
   @ApiModelProperty(value = "Invalid fields.")
-
-
   public Map<String, String> getFields() {
     return fields;
   }
@@ -72,67 +52,17 @@ public class Error   {
     this.fields = fields;
   }
 
-  public Error message(String message) {
-    this.message = message;
-    return this;
-  }
-
   /**
    * Get message
    * @return message
   **/
   @ApiModelProperty(value = "")
-
-
   public String getMessage() {
     return message;
   }
 
   public void setMessage(String message) {
     this.message = message;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Error error = (Error) o;
-    return Objects.equals(this.code, error.code) &&
-        Objects.equals(this.fields, error.fields) &&
-        Objects.equals(this.message, error.message);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(code, fields, message);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Error {\n");
-    
-    sb.append("    code: ").append(toIndentedString(code)).append("\n");
-    sb.append("    fields: ").append(toIndentedString(fields)).append("\n");
-    sb.append("    message: ").append(toIndentedString(message)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
   }
 }
 
